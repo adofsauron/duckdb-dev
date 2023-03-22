1488fb99d93bc54a6492b12397748a9e91759bdb
adofsauron <adofsauron@163.com>
2023-03-22 20:28:08 +0800

    fix

---
+++ trunk/dml.sql
@@ -0,0 +1,36 @@
+
+use memory;
+
+create table b(b1 int, b2 varchar(2), primary key(b1)) ;
+ 
+create table a(a1 int, a2 varchar(2), foreign key(a1) references b(b1)) ;
+ 
+create table c(c1 int, c2 varchar(2)) ;
+ 
+create table d(d1 int, d2 varchar(2)) ;
+
+insert into a values(1, 'a1');
+ 
+insert into a values(null, 'a2');
+ 
+insert into a values(3, 'a3');
+ 
+insert into b values(1, 'b1');
+ 
+insert into b values(2, 'b2');
+ 
+insert into b values(3, 'b3');
+ 
+insert into c values(1, 'c1');
+ 
+insert into c values(2, 'c2');
+ 
+insert into c values(null, 'c3');
+ 
+insert into d values(1, 'd1');
+ 
+insert into d values(2, 'd2');
+ 
+insert into d values(null, 'd3');
+
+
--- trunk/duckdb-0.7.1/examples/embedded-c++/main.cpp
+++ trunk/duckdb-0.7.1/examples/embedded-c++/main.cpp
@@ -7,8 +7,19 @@ int main() {
 
 	Connection con(db);
 
-	con.Query("CREATE TABLE integers(i INTEGER)");
-	con.Query("INSERT INTO integers VALUES (3)");
-	auto result = con.Query("SELECT * FROM integers");
+	// con.Query("CREATE TABLE integers(i INTEGER)");
+	// con.Query("INSERT INTO integers VALUES (3)");
+	// auto result = con.Query("SELECT * FROM integers");
+
+
+	con.Query("CREATE TABLE mytable(i INTEGER, j INTEGER)");
+	con.Query("CREATE TABLE myothertable(k INTEGER)");
+
+	con.Query("INSERT INTO mytable VALUES (1, 3)");
+	con.Query("INSERT INTO myothertable VALUES (1)");
+
+	 auto result = con.Query(
+	    "SELECT a, b + 1, c + 2 FROM (SELECT COUNT(*), SUM(i), SUM(j) FROM mytable WHERE i > 2) tbl(a, b, c)");
+
 	result->Print();
 }
